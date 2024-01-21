100fa7063b78f1b33f3057a347a98976c50dd854
YashK2005 <yashkoth7@gmail.com>
2024-01-20 22:45:47 -0500

    Made some nice navbar changes, now has fixed position and links change colour depending on what page currently on

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,21 +1,68 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./NavBar.css"; // Make sure to create a NavBar.css file in the same folder
+import React, { useEffect, useState } from "react";
+import github from "./github.png";
+import linkedin from "./linkedin.png";
+import "./NavBar.css";
 
 const NavBar = () => {
   const scrollToSection = (sectionId: string) => {
     document.getElementById(sectionId)?.scrollIntoView({ behavior: "smooth" });
   };
 
+  // State to track the current active link
+  const [activeLink, setActiveLink] = useState("home");
+
+  // Function to determine the active link based on the current URL
+  const getActiveLink = (linkId: string) => {
+    return activeLink === linkId ? "active" : "inactive";
+  };
+
+  // Function to handle URL changes and update the active link
+  const handleUrlChange = () => {
+    const currentPath = window.location.hash.substr(1); // Remove the '#' symbol
+    setActiveLink(currentPath || "home"); // Default to "home" if no hash
+  };
+
+  // Use useEffect to call handleUrlChange when the URL changes
+  useEffect(() => {
+    handleUrlChange();
+    window.addEventListener("hashchange", handleUrlChange);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("hashchange", handleUrlChange);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
-      <a id="name" href="#home">
+    <nav className={`navbar ${getActiveLink("home")}`}>
+      <a id="name" className={getActiveLink("home")} href="#home">
         Yash Kothari
       </a>
       <div>
-        <a href="#about">About Me</a>
-        <a href="#skills">Skills & Achievements</a>
-        <a href="#projects">Projects</a>
+        <a className={getActiveLink("about")} href="#about">
+          About Me
+        </a>
+        <a className={getActiveLink("skills")} href="#skills">
+          Skills & Achievements
+        </a>
+        <a className={getActiveLink("projects")} href="#projects">
+          Projects
+        </a>
+        {/* Add GitHub and LinkedIn icons with links */}
+        <a
+          href="https://github.com/YashK2005"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={github} alt="GitHub" className="social-icon" />
+        </a>
+        <a
+          href="https://linkedin.com/in/yashkothari7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedin} alt="LinkedIn" className="social-icon" />
+        </a>
       </div>
     </nav>
   );
